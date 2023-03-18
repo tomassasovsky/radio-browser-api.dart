a4d3c19d2cccb9d3a53a1701052599a264ebc734
Tomás Sasovsky <to.sasovsky@gmail.com>
2023-03-18 01:23:21 +0100

    fix: exports

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.1.0+2
+
+- fix: exported all models
+
 # 0.1.0+1
 
 - feat: initial commit 🎉
--- lib/radio_browser_api.dart
+++ lib/radio_browser_api.dart
@@ -1,4 +1,19 @@
 /// An HTTP client for Radio Browser
 library radio_browser_api;
 
+export 'src/models/codec.dart';
+export 'src/models/country.dart';
+export 'src/models/input_parameters.dart';
+export 'src/models/language.dart';
+export 'src/models/list_response.dart';
+export 'src/models/state.dart';
+export 'src/models/station.dart';
+export 'src/models/station_add.dart';
+export 'src/models/station_check.dart';
+export 'src/models/station_check_step.dart';
+export 'src/models/station_click.dart';
+export 'src/models/station_click_counter.dart';
+export 'src/models/station_vote.dart';
+export 'src/models/tag.dart';
+
 export 'src/radio_browser_api.dart';
