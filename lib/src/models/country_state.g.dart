e42859d5fc605a40ceacb48701aeafd3685e4921
Steve Gehrman <sgehrman@gmail.com>
2023-11-01 20:32:53 -0700

    renamed State

--- lib/src/models/country_state.g.dart
+++ lib/src/models/country_state.g.dart
@@ -1,18 +1,19 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'state.dart';
+part of 'country_state.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-State _$StateFromJson(Map<String, dynamic> json) => State(
+CountryState _$CountryStateFromJson(Map<String, dynamic> json) => CountryState(
       country: json['country'] as String,
       name: json['name'] as String,
       stationCount: json['stationcount'] as int,
     );
 
-Map<String, dynamic> _$StateToJson(State instance) => <String, dynamic>{
+Map<String, dynamic> _$CountryStateToJson(CountryState instance) =>
+    <String, dynamic>{
       'name': instance.name,
       'stationcount': instance.stationCount,
       'country': instance.country,
